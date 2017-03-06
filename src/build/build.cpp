c0abf607d2cfb1c84f75b13d835f4cf0e73df2da
xiwen.yxw <xiwen.yxw@alibaba-inc.com>
2017-03-06 19:20:11 +0800

    build

--- src/build/build.cpp
+++ src/build/build.cpp
@@ -1,3 +1,78 @@
 #include "build.h"
+#include "../common/ys_datapool.h"
 
+build::build() {
+    docIndex = 0;
+}
+
+build::~build() {
+}
+
+singleBuild::singleBuild() {
+}
+
+singleBuild::~singleBuild() {
+}
+
+void singleBuild::addDoc(char* buf, int len, int orgId) {
+    int docId = docIndex++;
+    dataPool<indexData> iDataPool;
+    dataPool<positMeta> pMetaPool;
+    dataPool<positData> pDataPool;
+    for (int i = 0; i < len; i += 4) {
+        if (pDataPool.getCurLen() > MAXLEN) {
+            writeIndex(iDataPool);
+            writePositMeta(pMetaPool);
+            writePositData(pDataPool);
+        }
+        indexMeta iData;
+        positMeta pMeta;
+        iData.docId = docId;
+        iData.offset = i;
+        iDataPool.push_back(iData);
+        pMeta.docId = docId;
+        pMeta.orgId = orgId;
+        pMeta.offset = pDataPool.getCurLen();
+        pMeta.length = len;
+        pMetaPool.push_back(pMeta);
+        pDataPool.push_back(buf, len);
+    }
+}
+
+void singleBuild::writeIndex(dataPool<indexData>& iDataPool) {
+    iDataPool.sort();
+    int curDocId = iDataPool[0].docId;
+    int curoffset = 0;
+    int curlength = 0;
+    for (int i = 0; i < iDataPool.getCurLen(); ++i) {
+        if (iDataPool[i].docId != curDocId) {
+            fwrite(&iDataPool[i].docId, sizeof(int), 1, file1);
+            fwrite(&curoffset, sizeof(int), 1, file1);
+            fwrite(&curlength, sizeof(int), 1, file1);
+            curoffset += curlength;
+            curlength = 0;
+        }
+        fwrite(&iDataPool[i].docId, sizeof(int), 1, file2);
+        fwrite(&iDataPool[i].offset, sizeof(int), 1, file2);
+        curlength ++;
+    }
+}
+
+void singleBuild::writePMeta(dataPool<positMeta>& pMetaPool) {
+    for (int i = 0; i < pMetaPool.getCurLen(); ++i) {
+        fwrite(&pMetaPool[i].docId, sizeof(int), 1, file1);
+        fwrite(&pMetaPool[i].orgId, sizeof(int), 1, file1);
+        fwrite(&pMetaPool[i].offset, sizeof(int), 1, file1);
+        fwrite(&pMetaPool[i].length, sizeof(int), 1, file1);
+    }
+    file1.close();
+}
+
+
+void singleBuild::writePData(dataPool<positData>& pDataPool) {
+    for (int i = 0; i < pMetaPool.getCurLen(); ++i) {
+        fwrite(&pDataPool[i], sizeofpositData(, 1, file));
+    }
+    file.close;
+}
 
--- src/build/build.h
+++ src/build/build.h
@@ -3,15 +3,56 @@
 
 namespace ys {
 
+struct positMeta {
+    int docId;
+    int orgId;
+    long offset;
+};
+
+struct indexData {
+    int docId;
+    int offset;
+};
+
+struct indexMeta {
+    int offset;
+    int length;
+};
+
+typedef char positData;
+
 class build {
 public:
     build();
-    ~build();
+    virtual ~build();
+public:
+    virtual void addDoc(char* buf, int len, int orgId) = 0;
+protected:
+    int docIndex;
 private:
     build(const build&);
     build& operator=(const build&);
 };
 
+class singleBuild : public build {
+public:
+    singleBuild();
+    ~singleBuild();
+public:
+    virtual void addDoc(char* buf, int len, int orgId);
+private:
+    void writeIndex(dataPool<indexData> iDataPool);
+    void writePMeta(dataPool<positMeta> pMetaPool);
+    void writePData(dataPool<positData> pDataPool);
+};
+
+class mutilBuild : public build {
+public:
+    mutilBuild();
+    ~mutilBuild();
+public:
+    virtual void addDoc(char* buf, int len, int orgId);
+};
 
 }
 
